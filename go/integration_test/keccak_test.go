ee1a96fd9de38677340738d28a92c8981c02adad
Facu <facumolina15@gmail.com>
2025-06-23 09:43:57 +0200

    Update license to Sonic (#102)
    * Update license and license header
    * Update license header in all relevant files

--- go/integration_test/keccak_test.go
+++ go/integration_test/keccak_test.go
@@ -1,7 +1,7 @@
-// Copyright (c) 2024 Fantom Foundation
+// Copyright (c) 2025 Sonic Operations Ltd
 //
 // Use of this software is governed by the Business Source License included
-// in the LICENSE file and at fantom.foundation/bsl11.
+// in the LICENSE file and at soniclabs.com/bsl11.
 //
 // Change Date: 2028-4-16
 //
